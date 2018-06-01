ca5d79f3493068ac503399b5bfbbd28588e4c398
John Waymire <waymire68@yahoo.com>
2018-05-31 20:49:13 -0400

    Merged in latest changes and installed NPM packages

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const jsCardRoutes = require('./routes/jsCardRoutes.js');
 const htmlCardRoutes = require('./routes/htmlCardRoutes.js');
 const cssCardRoutes = require('./routes/cssCardRoutes.js');
+const db = require('./models');
 
 const PORT = 3000;
 
@@ -16,15 +17,14 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-
-app.use('/', routes);
-app.use('/cards', cardRoutes);
+// app.use('/', routes);
+// app.use('/cards', cardRoutes);
 app.use(jsCardRoutes);
 app.use(htmlCardRoutes);
 app.use(cssCardRoutes);
 
-db.sequelize.sync().then(function(){
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log('app is now listening on port: ' + PORT);
-  });  
+  });
 });
