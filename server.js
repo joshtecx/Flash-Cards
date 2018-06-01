cf6d309996993740ed835f5a4afa928664342f03
John Waymire <waymire68@yahoo.com>
2018-05-31 20:42:48 -0400

    Commented out cardRoutes

--- server.js
+++ server.js
@@ -2,24 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const cookieParser = require("cookie-parser");
 const exphbs = require('express-handlebars');
-const mainRoutes = require('./routes/mainRoutes.js');
-const cardRoutes = require('./routes/cardsRoutes.js');
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
-app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
 
-app.use('/', routes);
-app.use('/cards', cardRoutes);
+const mainRoutes = require('./routes/mainRoutes.js');
+// const cardRoutes = require('./routes/cardsRoutes.js');
+// app.use('/cards', cardRoutes);
+app.use('/', mainRoutes);
 
 app.listen(PORT, function() {
   console.log('app is now listening on port: ' + PORT);
