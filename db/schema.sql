f1a4a3a24969d3059f961fe40968a07151b0c7f4
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-06-04 18:11:48 -0400

    updated schema and seeds....the wrong way

--- db/schema.sql
+++ db/schema.sql
@@ -12,11 +12,25 @@ CREATE TABLE Decks(
 );
 
 CREATE TABLE Cards(
+    CREATE TABLE Cards(
     id INT NOT NULL AUTO_INCREMENT,
     deck_name VARCHAR(200) NOT NULL,
-    question VARCHAR (250) NOT NULL,
-    hint VARCHAR(200) NOT NULL,
-    answer VARCHAR(250) NOT NULL,
+    question VARCHAR (250) ,
+    hint VARCHAR(200) ,
+    answer VARCHAR(250) ,
+    question2 VARCHAR (250) ,
+    hint2 VARCHAR(200) ,
+    answer2 VARCHAR(250) ,
+    question3 VARCHAR (250) ,
+    hint3 VARCHAR(200) ,
+    answer3 VARCHAR(250) ,
+    question4 VARCHAR (250) ,
+    hint4 VARCHAR(200) ,
+    answer4 VARCHAR(250) ,
+    question5 VARCHAR (250) ,
+    hint5 VARCHAR(200) ,
+    answer5 VARCHAR(250) ,
+    
     PRIMARY KEY (id),
-    FOREIGN KEY (deck_name) REFERENCES Decks(deck_name);
+    FOREIGN KEY (deck_name) REFERENCES Decks(deck_name)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,20 +8,9 @@ VALUES
 
 
 
-INSERT INTO Cards(deck_name, question, hint, answer)
+INSERT INTO Cards(deck_name, question, hint, answer, question2, hint2, answer2,question3, hint3, answer3, question4, hint4, answer4, question5, hint5, answer5)
 VALUES
-("JavaScript", "What language are Express apps written in?", "It starts with a 'J' ", "JavaScript"),
-("JavaScript", "What is one way a website can store data in a user's browser?", "They are delicious with milk", "Cookies"),
-("JavaScript", "What is a common way to shorten the response object's name inside middleware?", "It has the same abbreviation as 'Resolution'", "res"),
-("JavaScript", "How many different values can booleans have?", "Think: binary", "2"),
-("JavaScript", "Which HTML element can contain JavaScript?", "It starts with an 'S' ", "<script>"),
-("Html", "What does HTML stand for?", "It starts with a 'Hyper' ", "Hyper Text Markup Language"),
-("Html", "< br  / >What type of tag is this?", "Think Kit Kat", "Break"),
-("Html", "What tag is used to connect HTML and CSS?", "Zelda", "Link"),
-("Html", "What is the correct HTML element to define important text?", "Lift weights to get...", "Strong"),
-("Html", "Which HTML element can contain JavaScript?", "It starts with an 'S' ", "<script>"),
-("Css", "What does CSS stand for?", "_____ Style Sheets ", "Cascading Style Sheets"),
-("Css", "What symbol is used to reference an ID?", "They are huge on social media", " # "),
-("Css", "Which CSS property controls the text size?", " it is NOT text-size ", " font-size "),
-("Css", "What symbol is used to reference a CLASS ?", " Very commonly used punctuation mark ", " . "),
-("Css", "How do you specify the right margin of an element?", "This is super self-explanatory", "margin-right");
+("JavaScript", "What language are Express apps written in?", "It starts with a 'J' ", "JavaScript", "What is one way a website can store data in a user's browser?", "They are delicious with milk", "Cookies", "What is a common way to shorten the response object's name inside middleware?", "It has the same abbreviation as 'Resolution'", "res", "How many different values can booleans have?", "Think: binary", "2", "Which HTML element can contain JavaScript?", "It starts with an 'S' ", "<script>"),
+("Html", "What does HTML stand for?", "It starts with a 'Hyper' ", "Hyper Text Markup Language", "< br  / >What type of tag is this?", "Think Kit Kat", "Break", "What tag is used to connect HTML and CSS?", "Zelda", "Link", "What is the correct HTML element to define important text?", "Lift weights to get...", "Strong", "Which HTML element can contain JavaScript?", "It starts with an 'S' ", "<script>"),
+("Css", "What does CSS stand for?", "_____ Style Sheets ", "Cascading Style Sheets", "What symbol is used to reference an ID?", "They are huge on social media", " # ",   "Which CSS property controls the text size?", " it is NOT text-size ", " font-size ", "What symbol is used to reference a CLASS ?", " Very commonly used punctuation mark ", " . ", "How do you specify the right margin of an element?", "This is super self-explanatory", "margin-right")
+
