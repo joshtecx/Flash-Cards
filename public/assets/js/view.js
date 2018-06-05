7969e762f349bef0e54e0ed1afe721064515849f
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-06-05 00:31:15 -0400

    updated views.js...still not working though

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,8 +1,16 @@
-// $(document).ready(function() {
-//   $('#play-deck').on('click', function(event) {
-//     event.preventDefault();
+$(document).ready(function() {
+  $('#play-deck').on('click', function(event) {
+    event.preventDefault();
+    var newDeck = $("play-deck");
+    console.log(newDeck);
+    $.ajax("/api/cards/:deck_name", {
+        type: "GET",
+        data: newDeck
+    }).then(function(){
+        console.log(newDeck);
+        window.location.replace('/play');
+        console.log(newDeck);
+    });
 
-//     window.location.replace('/play');
-
-//   });
-// });
+  });
+});
