d4f0935a656bc24ac4a92f2a33df200f867242ec
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-06-05 17:25:51 -0400

    fixed something that I broke in views.js...I think

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,23 +1,7 @@
 $(document).ready(function() {
   $('#play-deck').on('click', function(event) {
     event.preventDefault();
-<<<<<<< HEAD
-    var newDeck = $("#deck").val();
-=======
-<<<<<<< 7969e762f349bef0e54e0ed1afe721064515849f
-    var newDeck = $("play-deck");
->>>>>>> e2c88c5b797d2b101e2aa63e8c734f36d7a0a38d
-    console.log(newDeck);
-    $.ajax("/api/cards/:deck_name", {
-        type: "GET",
-        data: newDeck
-    }).then(function(){
-        window.location.replace('/play');
-        JSON.stringify(newDeck);
-    });
-
-=======
-
+    
     var newDeck = $('#play-deck');
 
     $.ajax('/api/cards/:deck_name', {
@@ -30,6 +14,6 @@ $(document).ready(function() {
     }).then(function() {
       window.location.replace('/play');
     });
->>>>>>> read.me
+
   });
 });
